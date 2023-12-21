641bdf23b8aa2f37fe20e14e629862efd8cfe546
Audun Ytterdal <audun@ytterdal.net>
2023-12-21 13:29:58 +0100

    Adding more things

--- chaosbackend.go
+++ chaosbackend.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"html/template"
 	"log"
@@ -12,7 +13,7 @@ import (
 )
 
 type Config struct {
-	MainListen     string `json:"mainListen"`
+	listen         string `json:"mainListen"`
 	FailOverListen string `json:"failOverListen"`
 }
 
@@ -20,7 +21,7 @@ func readConfig() Config {
 	file, err := os.Open("config.json")
 	if err != nil {
 		log.Println("Error opening config file, using default values:", err)
-		return Config{MainListen: "127.0.0.1:8080", FailOverListen: "127.0.0.1:8081"} // Default values
+		return Config{listen: "127.0.0.1:8080", FailOverListen: "127.0.0.1:8081"} // Default values
 	}
 	defer file.Close()
 
@@ -29,7 +30,7 @@ func readConfig() Config {
 	err = decoder.Decode(&config)
 	if err != nil {
 		log.Println("Error decoding config file, using default values:", err)
-		return Config{MainListen: "127.0.0.1:8080", FailOverListen: "127.0.0.1:8081"} // Default values
+		return Config{listen: "127.0.0.1:8080", FailOverListen: "127.0.0.1:8081"} // Default values
 	}
 	return config
 }
@@ -48,6 +49,7 @@ func slowResponse(w http.ResponseWriter, sleepBeforeFirstByte time.Duration, sle
 	content := "Example content delivered slowly. Connect:" + sleepBeforeFirstByte.String() + " Betweenbytes:" + sleepBetweenBytes.String()
 	log.Println(content)
 	time.Sleep(sleepBeforeFirstByte)
+	w.Header().Set("Content-Type", "text/event-stream")
 	flusher, ok := w.(http.Flusher)
 	if !ok {
 		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
@@ -58,29 +60,49 @@ func slowResponse(w http.ResponseWriter, sleepBeforeFirstByte time.Duration, sle
 		flusher.Flush() // Manually flush the buffer
 		time.Sleep(sleepBetweenBytes)
 	}
-	log.Println("Done")
 }
 
 func slowHandler(w http.ResponseWriter, r *http.Request) {
-	sleepBeforeFirstByte, _ := strconv.Atoi(r.URL.Query().Get("sleepBeforeFirstByte"))
+	sleepBeforeFirstByte, _ := strconv.Atoi(r.URL.Query().Get("sleep"))
 	sleepBetweenBytes, _ := strconv.Atoi(r.URL.Query().Get("sleepBetweenBytes"))
 	slowResponse(w, time.Duration(sleepBeforeFirstByte)*time.Millisecond, time.Duration(sleepBetweenBytes)*time.Millisecond)
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request) {
-	statusCode, _ := strconv.Atoi(r.URL.Query().Get("statusCode"))
+	statusCode, _ := strconv.Atoi(r.URL.Query().Get("status"))
 	if statusCode == 0 {
-		http.Error(w, "This is an error", http.StatusInternalServerError)
-	} else {
-		http.Error(w, "Returning Error Statuscode: "+strconv.Itoa(statusCode), statusCode)
+		// Default to 500
+		statusCode = 500
 	}
+	sleepBeforeFirstByte, _ := strconv.Atoi(r.URL.Query().Get("sleep"))
+	log.Println("Returning Statuscode:", statusCode, "Sleeping for", sleepBeforeFirstByte, "ms")
+	time.Sleep(time.Duration(sleepBeforeFirstByte) * time.Millisecond)
+	http.Error(w, "Returning Statuscode: "+strconv.Itoa(statusCode), statusCode)
+}
+
+func resetConnectionHandler(w http.ResponseWriter, r *http.Request) {
+	// Take over the connection
+	conn, _, err := w.(http.Hijacker).Hijack()
+	if err != nil {
+		log.Printf("Hijacking failed: %v\n", err)
+		http.Error(w, "Hijacking failed", http.StatusInternalServerError)
+		return
+	}
+
+	// Close the connection immediately
+	conn.Close()
 }
 
 func main() {
+
 	config := readConfig()
+	flagListen := flag.String("listen", config.listen, "IP to listen on")
+	config.listen = *flagListen
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/slow", slowHandler)
 	mux.HandleFunc("/error", errorHandler)
+	mux.HandleFunc("/reset", resetConnectionHandler)
 	mux.HandleFunc("/", defaultHandler) // Register the default handler
 	failover_mux := http.NewServeMux()
 	failover_mux.HandleFunc("/", defaultHandler) // Register the default handler
@@ -88,6 +110,7 @@ func main() {
 		log.Println("Starting failover server", config.FailOverListen)
 		log.Fatal(http.ListenAndServe(config.FailOverListen, failover_mux))
 	}()
-	log.Println("Starting main server on", config.MainListen)
-	log.Fatal(http.ListenAndServe(config.MainListen, mux))
+	log.Println("Starting main server on", config.listen)
+
+	log.Fatal(http.ListenAndServe(config.listen, mux))
 }
